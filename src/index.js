884eee7b50082bc5c1bd272379ff87b17275ca48
katrinaiva <katrina_i@tut.by>
2021-03-12 00:27:37 +0300

    feat:  implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(array=[]) {
+  return array.length !== 0 ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array=[]) {
+  return array.length !== 0 ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array=[]) {
+  if (array.length !== 0) {
+    return array.reduce((sum, a) => sum + a, 0) / array.length;
+  } else {
+    return 0;
+  }
+};
